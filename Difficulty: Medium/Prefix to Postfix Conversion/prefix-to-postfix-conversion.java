85092f028e329b285af0ddc757949de58d6578e6
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-02 15:13:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+class Solution {
+        static String preToPost(String pre_exp) {
+        // code here
+        Stack<String> stack = new Stack<>();
+        for (int i = pre_exp.length()-1;i >=0 ;i--){
+            char ch = pre_exp.charAt(i);
+            String exp = ch+"";
+            if (isOperator(ch)){
+                exp = stack.pop()+stack.pop()+ch;
+            }
+            stack.push(exp);
+        }
+        return stack.pop();
+    }
+    private static boolean isOperator(char ch){
+        return ch=='^'||ch=='*'||ch=='/'||ch=='+'||ch=='-';
+    }
+}
