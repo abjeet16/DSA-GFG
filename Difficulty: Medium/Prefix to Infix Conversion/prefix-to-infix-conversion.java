9b4252dbc31cf5c9200b2dafb087bc0d148f0845
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-02 15:14:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+    static String preToInfix(String pre_exp) {
+        // code here
+        Stack<String> stack = new Stack<>();
+        for (int i = pre_exp.length()-1; i >=0 ;i--){
+            char ch = pre_exp.charAt(i);
+            String exp;
+            if (isOperator(ch)){
+                exp = "("+stack.pop() + ch + stack.pop()+")";
+            }else{
+                exp = ""+ch;
+            }
+            stack.push(exp);
+        }
+        return stack.pop();
+    }
+    private static boolean isOperator(char ch){
+        return ch=='^'||ch=='*'||ch=='/'||ch=='+'||ch=='-';
+    }
+}
