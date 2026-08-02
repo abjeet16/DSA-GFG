a602b601b95bdf6fd6a3a7d789d95aaf80d509df
abjeet yadav <avjeetyadav00@gmail.com>
2026-08-02 15:17:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from a sorted DLL/remove-duplicates-from-a-sorted-dll.java
@@ -0,0 +1,27 @@
+/*
+class Node{
+    int data;
+    Node next, prev;
+    Node(int x){
+        this.data = x;
+        this.next = null;
+        this.prev = null;
+    }
+}
+*/
+class Solution {
+    Node removeDuplicates(Node head) {
+        // Code Here.
+        Node curr = head;
+        while (curr!=null){
+            Node nxt = curr.next;
+            while (nxt!=null&&nxt.data==curr.data)
+                nxt = nxt.next;
+            curr.next = nxt;
+            if (nxt!=null)
+                nxt.prev = curr;
+            curr = nxt;
+        }
+        return head;
+    }
+}
